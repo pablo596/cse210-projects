9122ee8f8ee5f481e6bf7db4bceeaf0ec23ed58a
Pablo Palacios Álava <32400185+pablo596@users.noreply.github.com>
2025-04-10 14:47:30 -0500

    Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00} square units\n");
+        }
     }
 }
